98fb3327d687fac35937a31c4b1ef54152a173e0
Zahra Mardani <95300664+zahramardani@users.noreply.github.com>
2026-08-07 16:25:57 +0100

    Add Stage 3 generated API source

---
+++ replication/studies/study1_eight_participants/step3-ml2-integration/src/main/java/org/thingml/generated/api/IAnalyticsServer_fa_serviceClient.java
@@ -0,0 +1,7 @@
+package org.thingml.generated.api;
+
+import org.thingml.generated.api.*;
+
+public interface IAnalyticsServer_fa_serviceClient{
+void query_from_fa_service(double AnalyticsMsgs_query_feature1_var, double AnalyticsMsgs_query_feature2_var, double AnalyticsMsgs_query_feature3_var, double AnalyticsMsgs_query_target_feature_var);
+}
