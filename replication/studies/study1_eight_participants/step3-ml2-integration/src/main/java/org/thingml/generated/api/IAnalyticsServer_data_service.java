9c12d1f7edaa26a60ced344a96879e06d313d13c
Zahra Mardani <95300664+zahramardani@users.noreply.github.com>
2026-08-07 16:25:35 +0100

    Add Stage 3 generated API source

---
+++ replication/studies/study1_eight_participants/step3-ml2-integration/src/main/java/org/thingml/generated/api/IAnalyticsServer_data_service.java
@@ -0,0 +1,7 @@
+package org.thingml.generated.api;
+
+import org.thingml.generated.api.*;
+
+public interface IAnalyticsServer_data_service{
+void sensor_data_via_data_service(double AnalyticsMsgs_sensor_data_feature1_var, double AnalyticsMsgs_sensor_data_feature2_var, double AnalyticsMsgs_sensor_data_feature3_var, double AnalyticsMsgs_sensor_data_target_feature_var);
+}
